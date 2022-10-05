b778d976cd503ecaafcc70c1b5b6da8fa72e7914
Stultz-e <cstultz@oswego.edu>
2022-10-04 20:23:07 -0600

    push repo

---
+++ src/components/RelatedSongs.jsx
@@ -0,0 +1,31 @@
+import SongBar from './SongBar';
+
+const RelatedSongs = ({
+  handlePauseClick,
+  handlePlayClick,
+  data,
+  isPlaying,
+  activeSong,
+  artistId,
+}) => (
+  <div className='flex flex-col'>
+    <h1 className='font-bold text-3xl text-white'>Related Songs: </h1>
+
+    <div className='mt-6 w-full flex flex-col'>
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePlayClick={handlePlayClick}
+          handlePauseClick={handlePauseClick}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+export default RelatedSongs;
